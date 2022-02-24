a8f1a32d9c6d6005a085428313fa5c80e0ef9be0
Cristi Popolan <cpopolan@gmail.com>
2022-02-24 17:45:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] array = {1,2,3,4,5};
+        // int[] x = new int[5]; -sau asa cu una noua
+
+        for (var i:array) {
+            System.out.println(i);
+
+            // *************  sau  ****************
+       // for (int i=0;i< array.length;i++){
+        //} System.out.println(i);
+            // *************  end  ****************
+
+
+
+            // declarare o lista
+            List<Integer> nume_lista = new ArrayList<>();
+
+            //***************   Citite de la tastatura
+            Scanner scanner = new Scanner(System.in);
+
+            int var = scanner.nextInt();
+        // ****************  END
+
+            // transformare din String in INt
+
+            Integer.parseInt("Parametru string");
+
+            // *************** end
+    }
+
+}
